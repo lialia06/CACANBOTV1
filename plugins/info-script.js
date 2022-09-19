eea49c1b95e121b4c9d9abee4447bbc88f865ac8
liacans <104668197+lialia06@users.noreply.github.com>
2022-09-19 12:16:22 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://www.youtube.com/channel/*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
@@ -12,16 +12,16 @@ Mau Yang No Error Script Bot nya, Gas Cek Video YT :
 https://youtu.be/Pgv_GuQTPyo
 
 Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
+Bila ingin script bot ini kalian  bisa chat owner
 
-wa.me/6282127487538`
+wa.me/6288239560059`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://www.instagram.com/", 
+    title: 'YANS TIAN',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
