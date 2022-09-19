8595bfd5e22c6b1000603cb65c55b82ced62ee78
liacans <104668197+lialia06@users.noreply.github.com>
2022-09-19 12:14:38 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -66,10 +66,10 @@ https://github.com/ImYanXiao
 
 `
 conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/",
     mediaType: "VIDEO",
-    description: "https://Instagram.com/_ctzhid", 
-    title: 'WH-MODS-DEV-MultiDevice',
+    description: "https://Instagram.com/", 
+    title: 'Yans Tian',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
